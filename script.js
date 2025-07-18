32b7d1f5c86f08bdc62dd5e7c9c385de3dd6677c
Belseker <david.25.pt@gmail.com>
2025-07-18 22:20:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("unirseBtn").addEventListener("click", function () {
+  const tipo = document.getElementById("tipoTest").value;
+  alert("Has elegido un test de " + tipo + " preguntas");
+});
